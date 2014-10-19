295a3ca1f22c7b8343aef2468ca99f42fb86ba0b
Alberto Navarro <alnavarro@BookAir.local>
2014-10-19 08:15:57 -0400

    Updating for non available joints

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ var scraper = require('./scraper.js');
 module.exports = router;
 
 router.get('/',function(req,res){
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 
@@ -15,12 +15,17 @@ router.get('/json', function(req, res){  // Extra json link
    datetime = req.params.datetime || '10/18/2014 7:00%20PM'; // current time from requester.
    metroid = req.param.metroid || 4; //can be replaced to requester location
 
-   scraper(util.urlGenerator(cover,datetime,metroid),
-           function(err,table){
+   scraper(util.urlGenerator(cover,datetime,metroid,false),
+            function(err,table){
               if(err) res.send('err'); //probably insecure but for dev purpose left.
-              res.json(table);
-            }
-          );
+              var openTable = table;
+              scraper(util.urlGenerator(cover,datetime,metroid,true), // Totally forgot about this || toREFACTOR
+                function(err,table){
+                  if(err) res.send('err');
+                  var joinedTables = openTable.concat(table);
+                  res.json(cleanView(joinedTables));
+                });
+            });
 });
 
 
@@ -32,26 +37,40 @@ router.get('/csv', function(req, res){
 
    var converter = require('json-2-csv');
 
-   scraper(util.urlGenerator(cover,datetime,metroid),
-           function(err,table){
-
-              var toConvert = table.map(function(item){ // Cleaning view to adjust requirements.
-                return {                                // Given the time for the project, left here. toRefactor
-
-                          Name:item.name,
-                          Neighborhood:item.neighborhood,
-                          Cuisine:item.cuisine,
-                          Reviews: item.reviews,
-                          Window: item.windows.join('|'),
-                          Link: item.link
-
-                  };
-              });
-
-              converter.json2csv(toConvert, function(err,csv){ // convert to CSV
-                  res.send(new Buffer(csv));
-              });
-            }
-          );
+   scraper(util.urlGenerator(cover,datetime,metroid,false),
+            function(err,table){
+              if(err) res.send('err'); //probably insecure but for dev purpose left.
+              var openTable = table;
+              scraper(util.urlGenerator(cover,datetime,metroid,true), // Totally forgot about this || toREFACTOR
+                function(err,table){
+                  if(err) res.send('err');
+                  var joinedTables = openTable.concat(table);
+                  converter.json2csv(cleanView(table), function(err,csv){ // convert to CSV
+                      res.send(new Buffer(csv));
+                  });
+                });
+            });
 
 });
+
+
+function cleanView(dirtyArray){
+
+  return dirtyArray.map(function(item){ // Cleaning view to adjust requirements.
+
+    var cleanWindow = item.windows.join('|');
+    if(cleanWindow === '') cleanWindow = 0 ;
+    if(item.reviews === '') item.reviews = 'No Reviews'
+
+    return {
+              Name:item.name,
+              Neighborhood:item.neighborhood,
+              Cuisine:item.cuisine,
+              Reviews: item.reviews,
+              Window: cleanWindow,
+              Link: item.link
+      };
+  });
+
+
+}
--- util.js
+++ util.js
@@ -6,14 +6,15 @@ module.exports = {
                     return d.getMonth()+'/'+d.getDate()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes();
                   },
 
-  urlGenerator: function(cover,datetime,metroid){
+  urlGenerator: function(cover,datetime,metroid,full){
     //onlyunavailable=true
-    return 'http://www.opentable.com/s/?datetime=10/19/2014%207:00%20PM&covers=2&metroid=4&regionids=5&showmap=false&popularityalgorithm=NameSearches&tests=EnableMapview,ShowPopularitySortOption,srs,customfilters&sort=Popularity&excludefields=Description&';
+    //return 'http://www.opentable.com/s/?datetime=&covers=2&metroid=4&regionids=5&showmap=false&popularityalgorithm=NameSearches&tests=EnableMapview,ShowPopularitySortOption,srs,customfilters&sort=Popularity&excludefields=Description&';
 
                       return 'http://www.opentable.com/s/'
                               +'?datetime=' + datetime
                               +'&covers=' + cover
                               +'&metroid='+ metroid
+                              +'&onlyunavailable='+ full
                               +'&showmap=false&&popularityalgorithm=NameSearches&tests=EnableMapview,ShowPopularitySortOption,srs,customfilters&sort=Popularity&excludefields=Description';
   }
 
