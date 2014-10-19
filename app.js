18329ef8ce53d02fbbe74d2428a79a801a589a3e
Alberto Navarro <alnavarro@BookAir.local>
2014-10-18 21:25:06 -0400

    Modular implementation and Window Program

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app     = express();
-var sr = require(__dirname + '/scraper.js');
+var scraper = require('./scraper.js');
 
-app.get('/scrape', function(req, res){
+app.get('/scrape/:cover', function(req, res){
 
-   var table = sr('http://', function(err,table){
-    res.send('ok');
-   } );
+   cover = req.params.cover || 2; // OpenTable default.
+   datetime = req.param.datetime || '2014-10-18%2021:30'; // current time from requester.
+   metroid = req.param.metroid || 13; //can be replaced to requester location
+
+  //  var url  = 'http://www.opentable.com/s/? \
+  //                datetime=' + datetime + ' \
+  //                &covers=' + cover + ' \
+  //                &metroid='+ metroid +' \
+  //                &showmap=false' ;
+
+  var url = 'http://www.opentable.com/s/?datetime=10/18/2014%207:00%20PM&covers=2&metroid=4&regionids=5&showmap=false&popularityalgorithm=NameSearches&tests=EnableMapview,ShowPopularitySortOption,srs,customfilters&sort=Popularity&excludefields=Description'; 
+   scraper(url, function(err,table){ res.json(table); });
 
 })
 
 app.listen('8081')
 
-console.log('Magic happens on port 8081');
+function dateFomat(){
+  var d = new Date();
+  return d.getMonth()+'/'+d.getDate()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes();
+}
 
+function urlGenerator(){
+
+}
+
+
+
+console.log('Port 8081');
 exports = module.exports = app;
--- scraper.js
+++ scraper.js
@@ -3,9 +3,6 @@ var cheerio = require('cheerio');
 
 module.exports = function(url, fn) {
 
-  url = 'http://www.opentable.com/s/?datetime=10/18/2014%207:30%20PM&covers=2&metroid=4&regionids=5&showmap=false&popularityalgorithm=NameSearches';
-
-
   request(url, function(error, response, html){
         if(!error && response.statusCode == 200 ){ // No Errors
 
@@ -20,28 +17,76 @@ module.exports = function(url, fn) {
               var reviews       = $(element).find('.reviews').text().trim();
               var link          = $(element).find('.rest-content a').attr('href');
               var times_raw     = $(element).find('.timeslots li span.time');
-              var times = [];
+              var times         = [];
 
               times_raw.each(function(i){
-                  times.push($(this).text().trim());
+                   times.push( $(this).text().trim().replace(" PM", "") );
               });
 
-              if(name === '') return;
+              var windows = windowCalculator(times);
+
+              if(name === '' ) return;
+
 
               table.push({
                            name: name,
                            neighborhood: hood_cuisine[0],
                            cuisine: hood_cuisine[1],
                            reviews: reviews,
+                           times: times,
+                           windows: windowCalculator(times),
                            link: link,
-                           times: times
                            });
 
             });
-            console.log(table)
-            //executefn
-            fn();
-    }
+
+            fn(null,table);
+
+    // In case of an error with the page.
+    }else{  fn(true,error); }
+
   });
+
+
+  // Assume the start point is   4:59pm
+  // Assume the latest point is 10:01pm
+  function windowCalculator(times){
+    var start = '4:59', ends = '10:01', prev;
+    var windows = []; var flag = false;
+
+    prev = start;
+
+    for(var i = 0; i < times.length; i++){
+
+
+      if (times[i] == ''){
+          flag = true;
+      }else{
+          if(flag){
+              windows.push({w:minuteDistance(prev,times[i]),p:prev ,t: times[i]});
+              flag = false;
+          }
+          prev = times[i];
+      }
+
+    }
+
+   if(flag){
+      windows.push({w:minuteDistance(prev,ends),p:prev ,t: ends});
+      flag = false;
+   }
+
+   return windows;
+
+    function minuteDistance(start,end){
+      var timeStart  = new Date("06/18/1989 " + start);
+      var timeEnd    = new Date("06/18/1989 " + end);
+
+      return Math.round( (timeEnd - timeStart) / 60000);
+
+    }
+
+
+  }
 
 }
