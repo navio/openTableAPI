be67a76e26035231c4e13ff04faaa3b517298d40
Alberto Navarro <alnavarro@BookAir.local>
2014-10-26 22:53:36 -0400

    Schema Creator | NOT Tested

--- openTable/index.js
+++ openTable/index.js
@@ -9,58 +9,64 @@ router.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
-router.get('/raw', function(req, res){  // Raw to debug and other projects
-
-   cover = req.query.cover || 2; // OpenTable default.
-   datetime = req.query.datetime || '10-25-2014 7:00%20PM'; // current time from requester.
-   metroid = req.query.cityid || 4; //can be replaced to requester location
-
-   async.map([util.urlGenerator(cover,datetime,metroid,'false'),
-               util.urlGenerator(cover,datetime,metroid,'true')],
-               scraper,
-               function(err,table){
-                 if(err) console.log('there was an error'+err);
-                 var tables = table[0].concat(table[1]);
-                 res.json(tables);
-               }
-             );
-
+router.get('/experiment', function(req,res){
+  scraper(function(){
+    res.json('das');
+  });
 });
 
-router.get('/json', function(req, res){
-
-   cover = req.query.cover || 2; // OpenTable's default.
-   datetime = req.query.datetime || '10-25-2014 7:00%20PM'; // current time from requester.
-   metroid = req.query.cityid || 4; //can be replaced to requester location || Challenge example
-
-   async.map([util.urlGenerator(cover,datetime,metroid,'false'),
-               util.urlGenerator(cover,datetime,metroid,'true')],
-               scraper,
-               function(err,table){
-                 if(err) console.log('there was an error'+err);
-                 var tables = table[0].concat(table[1]);
-                 res.json(util.cleanView(tables));
-               }
-             );
-});
-
-router.get('/csv', function(req, res){
-
-   cover = req.query.cover || 2;
-   datetime = req.query.datetime || '10-25-2014 7:00%20PM';
-   metroid = req.query.cityid || 4;
-
-   var converter = require('json-2-csv');
-
-   async.map([util.urlGenerator(cover,datetime,metroid,'false'),
-              util.urlGenerator(cover,datetime,metroid,'true')],
-              scraper,
-              function(err,table){
-                if(err) console.log('there was an error'+err);
-                var tables = table[0].concat(table[1]);
-                converter.json2csv(util.cleanView(table), function(err,csv){ // convert to CSV
-                    res.send(new Buffer(csv));
-                });
-              }
-            );
-});
+// router.get('/raw', function(req, res){  // Raw to debug and other projects
+//
+//    cover = req.query.cover || 2; // OpenTable default.
+//    datetime = req.query.datetime || '10-25-2014 7:00%20PM'; // current time from requester.
+//    metroid = req.query.cityid || 4; //can be replaced to requester location
+//
+//    async.map([util.urlGenerator(cover,datetime,metroid,'false'),
+//                util.urlGenerator(cover,datetime,metroid,'true')],
+//                scraper,
+//                function(err,table){
+//                  if(err) console.log('there was an error'+err);
+//                  var tables = table[0].concat(table[1]);
+//                  res.json(tables);
+//                }
+//              );
+//
+// });
+//
+// router.get('/json', function(req, res){
+//
+//    cover = req.query.cover || 2; // OpenTable's default.
+//    datetime = req.query.datetime || '10-25-2014 7:00%20PM'; // current time from requester.
+//    metroid = req.query.cityid || 4; //can be replaced to requester location || Challenge example
+//
+//    async.map([util.urlGenerator(cover,datetime,metroid,'false'),
+//                util.urlGenerator(cover,datetime,metroid,'true')],
+//                scraper,
+//                function(err,table){
+//                  if(err) console.log('there was an error'+err);
+//                  var tables = table[0].concat(table[1]);
+//                  res.json(util.cleanView(tables));
+//                }
+//              );
+// });
+//
+// router.get('/csv', function(req, res){
+//
+//    cover = req.query.cover || 2;
+//    datetime = req.query.datetime || '10-25-2014 7:00%20PM';
+//    metroid = req.query.cityid || 4;
+//
+//    var converter = require('json-2-csv');
+//
+//    async.map([util.urlGenerator(cover,datetime,metroid,'false'),
+//               util.urlGenerator(cover,datetime,metroid,'true')],
+//               scraper,
+//               function(err,table){
+//                 if(err) console.log('there was an error'+err);
+//                 var tables = table[0].concat(table[1]);
+//                 converter.json2csv(util.cleanView(table), function(err,csv){ // convert to CSV
+//                     res.send(new Buffer(csv));
+//                 });
+//               }
+//             );
+// });
--- openTable/scraper.js
+++ openTable/scraper.js
@@ -1,95 +1,150 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var async   = require('async');
 
 module.exports = function(url,cb) {
 
-    request(url, function(error, response, html){
+  cb(schemaReader(schema));
 
-        if(!error && response.statusCode == 200 ){ // No Errors
+  var schema = {
+    request : {
+      url: 'url.com/ass/asa',
+      method: 'get',
+      data : {
+        container:{
+            selector: '#search_results_table.table tr',
+            multiple: true,
+            children: {
+                name: '.rest-name',
+                hood_cuisine: '.rest-content div',
+                reviews: '.reviews',
+                link: { selector: '.rest-content a', attr: 'href' },
+                times: { selector: '.timeslots li span.time', multiple: true }
+            }
+        }
+      }
+    }
+  };
 
-            var $ = cheerio.load(html);
-                    cb(null,scrape($));
+  function schemaReader(elements){
 
-        }else{  cb(error,null) }
+    for( var element in elements){
+      if (!requestValidity(element)) return false;
+      request(element.url,
+              function(error, response, html){
+                              parser(html,element.data);
+              });
+    }
 
-    });
+    function requestValidity(item){
+      if(!item.url) return false;
+      if(!item.data) return false;
+    }
 
-  function scrape($){
+    function parser(data,schema){
+      var $ = cheerio.load(data);  // check to not create a new object everytime.
 
-        var table = [];
-        $('#search_results_table.table tr').each(function(i, element){
+      return schema.map(function(element){
 
-          var name          = $(element).find('.rest-name').text();
-          var hood_cuisine  = $(element).find('.rest-content div').text().split(" | ");
-          var reviews       = $(element).find('.reviews').text().trim();
-          var link          = $(element).find('.rest-content a').attr('href');
-          var times_raw     = $(element).find('.timeslots li span.time');
-          var times         = [];
+        var toSelect = element.selector ? element.selector: element; // simple leaf or not;
+        var el = $(toSelect);
 
-          if(name === '' ) return;
+        return el.map(function(i,element){
+          var finalValue = '';
 
-          times_raw.each(function(i){
-               times.push( $(this).text().trim().replace(" PM", "") );
-          });
+          if(element.attr){ //return attribute value
+            finalValue = el.attr(element.attr);
+          }else{
+            finalValue = el.text();
+          }
 
-          var windows = windowCalculator(times);
+          if(element.children){
+            return ['value': finalValue , 'children': parser(el,element.children) ]; // return object
+          }
 
-          table.push({
-                       name: name,
-                       neighborhood: hood_cuisine[0],
-                       cuisine: hood_cuisine[1],
-                       reviews: reviews,
-                       times: times,
-                       windows: windowCalculator(times),
-                       link: link,
-                       });
-
+          return finalValue;
         });
+      }
+    }
 
-        return table;
   }
+
+  // function scrape($){
+  //
+  //       var table = [];
+  //       $('#search_results_table.table tr').each(function(i, element){
+  //
+  //         var name          = $(element).find('.rest-name').text();
+  //         var hood_cuisine  = $(element).find('.rest-content div').text().split(" | ");
+  //         var reviews       = $(element).find('.reviews').text().trim();
+  //         var link          = $(element).find('.rest-content a').attr('href');
+  //         var times_raw     = $(element).find('.timeslots li span.time');
+  //         var times         = [];
+  //
+  //         if(name === '' ) return;
+  //
+  //         times_raw.each(function(i){
+  //              times.push( $(this).text().trim().replace(" PM", "") );
+  //         });
+  //
+  //         var windows = windowCalculator(times);
+  //
+  //         table.push({
+  //                      name: name,
+  //                      neighborhood: hood_cuisine[0],
+  //                      cuisine: hood_cuisine[1],
+  //                      reviews: reviews,
+  //                      times: times,
+  //                      windows: windowCalculator(times),
+  //                      link: link,
+  //                      });
+  //
+  //       });
+  //
+  //       return table;
+  // }
 
   // Assume the start point is   4:59pm
   // Assume the latest point is 10:01pm
   // Exist in here to not create depedency to utils
-  function windowCalculator(times){
-    if(times.length == 0) return [302];
-    var start = '4:59', ends = '10:01', prev;
-    var windows = []; var flag = false;
-
-    prev = start;
-
-    for(var i = 0; i < times.length; i++){
-
-
-      if (times[i] == ''){
-          flag = true;
-      }else{
-          if(flag){  // when window is closed.
-              windows.push(minuteDistance(prev,times[i]));
-              flag = false;
-          }
-          prev = times[i];
-      }
-
-    }
-
-   if(flag){ // final case.
-      windows.push(minuteDistance(prev,ends));
-      flag = false;
-   }
-
-   return windows;
-
-    function minuteDistance(start,end){
-      var timeStart  = new Date("06/18/1989 " + start);
-      var timeEnd    = new Date("06/18/1989 " + end);
-
-      return Math.round( (timeEnd - timeStart) / 60000);
-
-    }
-
-
-  }
-
-}
+//   function windowCalculator(times){
+//     if(times.length == 0) return [302];
+//     var start = '4:59', ends = '10:01', prev;
+//     var windows = []; var flag = false;
+//
+//     prev = start;
+//
+//     for(var i = 0; i < times.length; i++){
+//
+//
+//       if (times[i] == ''){
+//           flag = true;
+//       }else{
+//           if(flag){  // when window is closed.
+//               windows.push(minuteDistance(prev,times[i]));
+//               flag = false;
+//           }
+//           prev = times[i];
+//       }
+//
+//     }
+//
+//    if(flag){ // final case.
+//       windows.push(minuteDistance(prev,ends));
+//       flag = false;
+//    }
+//
+//    return windows;
+//
+//     function minuteDistance(start,end){
+//       var timeStart  = new Date("06/18/1989 " + start);
+//       var timeEnd    = new Date("06/18/1989 " + end);
+//
+//       return Math.round( (timeEnd - timeStart) / 60000);
+//
+//     }
+//
+//
+//   }
+//
+ }
